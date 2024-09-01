58e58b7d3b71c1acb69ec46f8ed31ae757581e8f
MalikUmair5 <malikumair.dev@gmail.com>
2024-09-02 00:54:09 +0500

    Continue working on Content SHowcase Section

--- designing with tailwind/Understanding Tailwind CSS/src/sections/ProductShowcase.tsx
+++ designing with tailwind/Understanding Tailwind CSS/src/sections/ProductShowcase.tsx
@@ -1,3 +1,40 @@
+import Image from "next/image";
+import pyramidImage from "@/assets/pyramid.png";
+import tubeimage from "@/assets/tube.png";
+import productImage from "@/assets/product-image.png";
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className="overflow-x-clip bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24">
+      <div className="container">
+        <div className="max-w-[540px] mx-auto">
+          <div className="flex justify-center">
+            <div className="tag">Boost Your Productivity</div>
+          </div>
+          <h2 className="md:text-[54px] md:leading-[60px] text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to to-[#001E80] text-transparent bg-clip-text">
+            A more effective to track Progress
+          </h2>
+          <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+            Effortlessly turn your Idea into fully functional responsive SaaS
+            Website in just minutes with this tempelate
+          </p>
+        </div>
+        <div className="relative">
+          <Image src={productImage} alt="product Image" className="mt-10" />
+          <Image
+            src={pyramidImage}
+            alt="pyramid Image"
+            className="absolute hidden md:block -right-36 -top-32"
+            height={262}
+          />
+          <Image
+            src={tubeimage}
+            alt="Tube Image"
+            className="absolute hidden md:block -left-36 -bottom-24"
+            height={262}
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
